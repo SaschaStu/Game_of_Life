a9d78935cf2aa7a4000315a11570c2b61ca5cb05
Dackeldu <42412228+Dackeldu@users.noreply.github.com>
2020-12-09 16:27:08 +0100

    This is a test

--- src/main/java/ch/bbw/pr/helloworldfx/Main.java
+++ src/main/java/ch/bbw/pr/helloworldfx/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-
+			System.out.println("Test");
 			Model model = new Model();
 
 			FXMLLoader myLoader = new FXMLLoader(getClass().getResource("View.fxml"));
