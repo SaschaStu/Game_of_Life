72dd13d240283158f6d554460085f43967055a16
SaschaStu <sascha.sturzenegger@stud.kbw.ch>
2020-12-09 16:32:46 +0100

    test

--- src/main/java/ch/bbw/pr/helloworldfx/Main.java
+++ src/main/java/ch/bbw/pr/helloworldfx/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			System.out.println("pp");
+			System.out.println("despacito");
 			Model model = new Model();
 			//test
 			FXMLLoader myLoader = new FXMLLoader(getClass().getResource("View.fxml"));
